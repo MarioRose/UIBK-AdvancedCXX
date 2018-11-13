a2b7626cb4a0f2564f373b7f7a8baa5a58799350
christoph <christoph.kirchner@student.uibk.ac.at>
2018-11-13 14:00:24 +0100

    added shared pointer

--- ex03/task1/task1.cpp
+++ ex03/task1/task1.cpp
@@ -34,20 +34,77 @@ public:
 private:
     //Copy assignment
     unique_ptr_to_vec2& operator= (const unique_ptr_to_vec2& rhs) {}
-    //Copy constuctor
+    //Copy constructor
     unique_ptr_to_vec2(const unique_ptr_to_vec2& u) {}
 
 };
 
+class shared_ptr_to_vec2 {
+
+    Vec2* vec;
+    int* counter;
+
+public:
+    shared_ptr_to_vec2() {
+        std::cout << "shared pointer created\n";
+        vec = new Vec2(0, 0);
+        counter = new int(1);
+    }
+
+    //Copy constructor
+    shared_ptr_to_vec2(const shared_ptr_to_vec2& u) {
+        vec = u.vec;
+        counter = u.counter;
+        *counter++;
+    }
+
+    ~shared_ptr_to_vec2() {
+        *counter--;
+        if(*counter == 0){
+            vec->~Vec2();
+            std::cout << "shared pointer destroyed\n";
+        }
+    }
+
+    Vec2& operator * (){
+        return *vec;
+    }
+
+    Vec2* operator->(){
+        return vec;
+    }
+
+    //Copy assignment
+    shared_ptr_to_vec2& operator= (const shared_ptr_to_vec2& rhs) {
+        return *this;
+    }
+
+
+};
+
+
 int main(int argc, char * argv[]){
 
     unique_ptr_to_vec2 testPointer;
+    shared_ptr_to_vec2 sharedTestPointer;
 
-    //unique_ptr_to_vec2 t = testPointer;
-    
+    shared_ptr_to_vec2 sharedTestPointer2 = sharedTestPointer;
+
+    sharedTestPointer2->x = 3;
+
+    {
+        shared_ptr_to_vec2 sharedTestPointer3 = sharedTestPointer;
+
+        std::cout <<  sharedTestPointer3.operator*().x << "\n";
+    }
+
+    std::cout <<  sharedTestPointer2.operator*().x << "\n";
+
+    sharedTestPointer->x = 33;
 
     {
         unique_ptr_to_vec2 testPointer;
+        std::cout <<  sharedTestPointer2.operator*().x << "\n";
     }
 
     std::cout << "Last Statement\n";
