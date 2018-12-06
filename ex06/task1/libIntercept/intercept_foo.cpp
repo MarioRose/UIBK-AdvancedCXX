56c2e4ae6168e1d9f87b7b9a968eb845bf4af545
christoph <christoph.kirchner@student.uibk.ac.at>
2018-12-06 23:08:23 +0100

    replaced memcopy with type cast

--- ex06/task1/libIntercept/intercept_foo.cpp
+++ ex06/task1/libIntercept/intercept_foo.cpp
@@ -14,18 +14,12 @@ int random_number()
 
 void just_a_print()
 {
-	std::cout << "some text before\n";
-
 	typedef void (*handle)();
 
-	static handle origMethod = 0;
-	if(origMethod == 0) {
-		void *tmpPtr = dlsym(RTLD_NEXT, "_ZN3foo12just_a_printEv");
+	std::cout << "some text before\n";
 
-		std::memcpy(&origMethod, &tmpPtr, sizeof(void*));
-	}
-
-	(*origMethod)();
+	handle print = (handle)dlsym(RTLD_NEXT, "_ZN3foo12just_a_printEv");
+	(*print)();
 
 	std::cout << "some text after\n";
 }
